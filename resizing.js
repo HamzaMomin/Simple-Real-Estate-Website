bd190e1c9a72f3d0d277237002206d6df45f19c7
Hamza Momin <65024078+HamzaMomin@users.noreply.github.com>
2022-07-21 16:08:57 +0500

    Add files via upload

---
+++ resizing.js
@@ -0,0 +1,12 @@
+
+function displayDate() {
+document.getElementById("demo").innerHTML = Date();}
+function aFunction(value) { alert("New Value: " + value); }
+function myOverFunction()
+{document.getElementById("z").style.fontSize = "50px";}
+function myOutFunction()
+{document.getElementById("z").style.fontSize = "medium";}
+function keydownFunction()
+{ document.getElementById("a").style.backgroundColor = "yellow";}
+function loadFunction() {
+alert("Page loaded");}
